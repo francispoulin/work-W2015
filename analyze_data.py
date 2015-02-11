782f08fe5be208607b158235caf8224adbf1a46c
Dakota St. Laurent <d.h.stlaurent@gmail.com>
2015-02-11 14:38:29 -0500

    add .pyc to gitignore; refactor analyze_data; create heat_2d_setup.py which initializes many common things in the heat-2d solvers; remove redundancy from file names for heat-2d; make use of heat_2d_setup.py; add numba solver for heat-2d

--- analyze_data.py
+++ analyze_data.py
@@ -1,26 +1,60 @@
 import numpy as np
-import scipy as sp
-import sys
 
 processors = [2, 4]
-Ms = range(8, 13)
+Ms = range(8, 11)
 D1 = ['par-step']
 D2 = ['ser-step']
 rootDir = 'heat-2d'
 
-# parallel files
-for d in D1:
-  for p in processors:
+
+def get_parallel_data(rootDir, procs, method, Ms, subDir=''):
+  """
+  example:
+    rootDir = 'heat-2d'
+    procs   = [2, 4]
+    method  = 'par-step'
+    subDir  = 'numba'       # for numba tests
+    Ms      = [8, 9, 10]
+  """
+  if subDir != '':
+    subDir += '/'
+
+  result = dict([(p, []) for p in procs])
+
+  for p in procs:
+    means = []
     for M in Ms:
-      filename = '%s/tests/%s/p%d-M%s.txt' % (rootDir, d, p, str(M).zfill(2))
+      filename = '%s/tests/%s%s/p%d-M%s.txt' % (rootDir, subDir, method, p, str(M).zfill(2))
 
       with open(filename) as f:
         data = [float(i) for i in f.readlines()]
+        means.append(np.mean(data))
 
-for d in D2:
+    result[p] = means
+
+  return result
+
+
+def get_serial_data(rootDir, method, Ms, subDir=''):
+  """
+  example:
+    rootDir = 'heat-2d'
+    method  = 'ser-step'
+    subDir  = 'numba'       # for numba tests
+    Ms      = [8, 9, 10]
+  """
+  if subDir != '':
+    subDir += '/'
+
   means = []
   for M in Ms:
-    filename = '%s/tests/%s/M%s.txt' % (rootDir, d, str(M).zfill(2))
+    filename = '%s/tests/%s%s/M%s.txt' % (rootDir, subDir, method, str(M).zfill(2))
 
     with open(filename) as f:
       data = [float(i) for i in f.readlines()]
+      means.append(np.mean(data))
+  print 's', means
+
+  return means
+
+print get_serial_data('heat-2d', 'ser-step', range(8,11), 'original')
